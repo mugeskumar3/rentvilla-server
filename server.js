13bbc288d598194b2a16f0ce251e584c59ca30c6
Mugesh R <mugeskumar3@gmail.com>
2024-05-26 23:28:25 +0530

    #chnage Node Port

--- server.js
+++ server.js
@@ -241,7 +241,7 @@ app.get('/api/properties', async (req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
